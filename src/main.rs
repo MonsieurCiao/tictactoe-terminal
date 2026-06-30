daf6421bb207bdd1e4b04be38c0df0fd8a884268
MonsieurCiao <140845297+MonsieurCiao@users.noreply.github.com>
2026-06-30 17:02:46 +0200

    working on ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,112 @@
+use std::io;
 use tictactoe::Game;
 
-fn main() {
-    let mut game = Game::new();
-    game.run();
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
+
+fn main() -> io::Result<()> {
+    ratatui::run(|terminal| App::default().run(terminal))
+
+    // let mut game = Game::new();
+    // game.run();
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    //here goes the state
+    counter: u8,
+    exit: bool,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event);
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+    }
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from("Counter App".bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<q>".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title)
+            .title_bottom(instructions)
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn handle_key_event() {
+        let mut app = App::default();
+        app.handle_key_event(KeyCode::Right.into());
+        assert_eq!(app.counter, 1);
+
+        app.handle_key_event(KeyCode::Left.into());
+        assert_eq!(app.counter, 0);
+
+        app.handle_key_event(KeyCode::Char('q').into());
+        assert!(app.exit);
+    }
 }
